6743ade20c9dfccac6ef126421c285044d4a929c
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-05-31 10:09:04 +0200

    Add tools ui on touch

---
+++ sketch_room/js/sketch_room/cauldron/show_on_thumbstick_touch.js
@@ -0,0 +1,19 @@
+WL.registerComponent('show_on_thumbstick_touch', {
+}, {
+    init: function () {
+    },
+    start: function () {
+        this.object.scale([0, 0, 0]);
+        this.object.setTranslationLocal([0, -7777, 0]);
+    },
+    update: function (dt) {
+        if (PP.RightGamepad.getButtonInfo(PP.ButtonType.THUMBSTICK).isTouchStart()) {
+            this.object.resetScaling();
+            this.object.resetTransform();
+
+        } else if (PP.RightGamepad.getButtonInfo(PP.ButtonType.THUMBSTICK).isTouchEnd()) {
+            this.object.scale([0, 0, 0]);
+            this.object.setTranslationLocal([0, -7777, 0]);
+        }
+    },
+});
